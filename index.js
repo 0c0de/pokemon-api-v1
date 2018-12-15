c4f9903da56b32148f1fd5e8494052668bcdda86
0c0de <fernandezmateojoseluis@gmail.com>
2018-12-15 18:34:48 +0100

    more

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 
 const app = express();
-const port = proccess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("Someone likes pokemons");
